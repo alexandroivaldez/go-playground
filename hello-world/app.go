a39af6410b6c3f0b15d1afad8a5591e8c1f898ab
Alexandro Valdez <alexandroivaldez@gmail.com>
2024-02-09 22:24:40 -0600

    Created first go module and build

--- hello-world/app.go
+++ hello-world/app.go
@@ -1,5 +1,9 @@
+// A package is required for every go project.
+// main is a special package name, that tells go that this is the main entry point.
+// basic main as any other language.
 package main
 
+// fmt is a package from go
 import "fmt"
 
 func main() {
