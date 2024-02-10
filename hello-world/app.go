17c4b4a649c6d801af489feaf173df8938cf6ece
Alexandro Valdez <alexandroivaldez@gmail.com>
2024-02-09 22:48:21 -0600

    Forgot to add root changes

--- hello-world/app.go
+++ hello-world/app.go
@@ -1,9 +1,9 @@
 // A package is required for every go project.
 // main is a special package name, that tells go that this is the main entry point.
-// basic main as any other language.
+// Acts like a basic main in any other language. Sort of like README.md, it's what's ran first.
 package main
 
-// fmt is a package from go
+// fmt is a native package from go
 import "fmt"
 
 func main() {
